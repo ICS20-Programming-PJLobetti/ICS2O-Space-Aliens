914bc6f0878a532d24bae4fb911f3005c6991488
PJLobetti31 <99275095+PJLobetti31@users.noreply.github.com>
2022-06-01 14:12:01 +0000

    added a scale to the game and a few comments.

--- js/game.js
+++ js/game.js
@@ -1,11 +1,30 @@
 /* global Phaser */
 
+// Copyright (c) 2022 PJ Lobetti All rights reserved
+//
+// Created by: PJ Lobetti
+// Created on: Jun 2022
+// This is the Phaser3 configuration file
+
+//* Game Scene */
 const config = {
   type: Phaser.AUTO,
   width: 1920,
   height: 1080,
-  backgroundColor: 0x5f6e7a
+  physics: {
+    default: 'arcade',
+    arcade: {
+      debug: true
+    }
+  },
+  // set background color
+  backgroundColor: 0x5f6e7a,
+  scale: {
+    mode: Phaser.Scale.FIT,
+    // we place it in the middle of the page
+    autoCenter: Phaser.Scale.CENTER_BOTH
+  }
 }
 
-const game = new Phaser.Game(config)
-console.log(game)
+  const game = new Phaser.Game(config)
+  console.log(game)
