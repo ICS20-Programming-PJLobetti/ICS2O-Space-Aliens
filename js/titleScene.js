4b0d1f19aecc8f25c6f911c13bb81a73d9bab2f4
PJLobetti31 <99275095+PJLobetti31@users.noreply.github.com>
2022-06-01 15:05:40 +0000

    stuff

---
+++ js/titleScene.js
@@ -0,0 +1,29 @@
+/* global Phaser */
+
+// Copyright (c) 2022 PJ Lobetti All rights reserved
+//
+// Created by: PJ Lobetti
+// Created on: Jun 2022
+// This is the Title Scene
+
+class TitleScene extends Phaser.Scene {
+  constructor() {
+    super({ key: 'titleScene' })
+  }
+
+  init(data) {
+    this.cameras.main.setBackgroundColor('#ffffff')
+  }
+
+  preload() {
+    console.log('Title Scene')
+  }
+
+  create(data) {
+  }
+
+  update(time, delta) {
+  }
+}
+
+  export default TitleScene
