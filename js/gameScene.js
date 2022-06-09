2d3cf3e44941227fc6b8e30ed5117b01c7d218ad
PJLobetti31 <99275095+PJLobetti31@users.noreply.github.com>
2022-06-09 14:22:01 +0000

    idk whats wrong

--- js/gameScene.js
+++ js/gameScene.js
@@ -7,10 +7,14 @@
 // This is the Game Scene
 
 class GameScene extends Phaser.Scene {
-
   // create an alien
   creatAlien () {
-    const anAlien = this.physics.add.sprite(100, 100, 'alien')
+    const alienXLocations = Math.floor(Math.random() * 1920) + 1
+    let alienXVelocity = Math.floor(Math.random() * 50) + 1
+    alienXVelocity *= Math.round(Math.random()) ? 1 : -1
+    const anAlien = this.physics.add.sprite(alienXLocation, -100, 'alien')
+    anAlien.body.velocity.y = 200
+    anAlien.body.velocity.x = alienXVelocity
     this.alienGroup.add.(anAlien)
   }
   
